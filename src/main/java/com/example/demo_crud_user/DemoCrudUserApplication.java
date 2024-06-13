1febb54b866aa40b9e57083d7a524c9d124dc510
DwngLee <156188368+DwngLee@users.noreply.github.com>
2024-06-13 14:03:31 +0700

    khoi tao repo

---
+++ src/main/java/com/example/demo_crud_user/DemoCrudUserApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_crud_user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCrudUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCrudUserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo_crud_user/DemoCrudUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_crud_user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCrudUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
